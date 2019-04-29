e05c31151d4188c6f3bd9a594693838ca5e29949
Siriwasan <siriwasan@gmail.com>
2019-04-30 00:26:13 +0700

    edit Header and Sidenav-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material.module';
@@ -9,18 +11,15 @@ import { HeaderComponent } from './core/header/header.component';
 import { SidenavListComponent } from './core/sidenav-list/sidenav-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SidenavListComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, SidenavListComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
-import { MatSidenavModule, MatButtonModule } from '@angular/material';
+import {
+  MatSidenavModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatSidenavModule],
-  exports: [MatButtonModule, MatSidenavModule]
+  imports: [MatButtonModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule],
+  exports: [MatButtonModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule]
 })
 export class MaterialModule {}
