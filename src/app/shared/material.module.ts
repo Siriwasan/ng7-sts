874abcdf648a26d41e67b1529565689c46cde03e
Siriwasan <siriwasan@gmail.com>
2019-04-30 00:00:49 +0700

    install Angular Material

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material';
+
+@NgModule({
+  imports: [MatSidenavModule],
+  exports: [MatSidenavModule]
+})
+export class MaterialModule {}
